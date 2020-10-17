0583a5e54eb5906c7fb9faaffe6eac8d3204978f
Jisan-Mia <jisanmia47@gmail.com>
2020-10-17 21:30:18 +0600

    added login page and other event handler related to login page

--- src/script.js
+++ src/script.js
@@ -34,7 +34,26 @@ function subtotalCalc(phonePrice,coverPrice, addOrDeductNumber ){
 	document.getElementById('subtotal').innerHTML = updateSubtotal;
 	console.log(updateSubtotal)
 }
-// function for mobile and mobile cover plus button
+
+//tax calculation function
+function taxCalc(tax, addOrDeductNum){
+	let taxNum = parseFloat(tax);
+	let updateTax = taxNum + addOrDeductNum;
+	tax = updateTax;
+	document.getElementById('tax').innerHTML = updateTax;
+	console.log(updateTax)
+}
+
+//total calculation function
+function totalCalc(subtotal, tax){
+		let subtotalNum = parseFloat(subtotal);
+		let taxNum = parseFloat(tax);
+		let updateTotal = subtotalNum + taxNum;
+		document.getElementById('total').innerHTML = updateTotal;
+	}
+
+
+// function for mobile and mobile cover plus and Minus button
 function allPlusMinusBtnFunc(countId,posOrNegOne, priceId, individualPrice, remainId){
 	//calc counter
 	let count = document.getElementById(countId).value;
@@ -48,6 +67,7 @@ function allPlusMinusBtnFunc(countId,posOrNegOne, priceId, individualPrice, rema
 	let remainPrice = document.getElementById(remainId).innerHTML;
 	subtotalCalc(price, remainPrice, individualPrice);
 }
+
 
 //plus button funtion
 function plusBtnFunc(){
@@ -66,6 +86,17 @@ function plusBtnFunc(){
 	let coverPrice = document.getElementById('coverPrice').innerHTML;
 
 	subtotalCalc(phonePrice, coverPrice, 1219)*/
+	//tax
+	let tax = document.getElementById('tax').innerHTML;
+	taxCalc(tax, 5);
+
+	// total
+	let subtotal = document.getElementById('subtotal').innerHTML;
+
+	// console.log(typeof updateTotal,updateTotal)
+	totalCalc(subtotal, tax);
+
+
 }
 
 //cover plus button function
@@ -87,6 +118,14 @@ function coverPlusBtnFunc(){
 	let phonePrice = document.getElementById('phonePrice').innerHTML;
 
 	subtotalCalc(phonePrice, coverPrice, 59)*/
+	//tax
+	let tax = document.getElementById('tax').innerHTML;
+
+	// total
+	let subtotal = document.getElementById('subtotal').innerHTML;
+	totalCalc(subtotal, tax);
+
+	
 
 }
 
@@ -109,6 +148,18 @@ function minusBtnFunc(){
 		let coverPrice = document.getElementById('coverPrice').innerHTML;
 
 		subtotalCalc(phonePrice, coverPrice, -1219)*/
+		//tax
+		let tax = document.getElementById('tax').innerHTML;
+		taxCalc(tax, -5)
+
+		// total
+		let total = document.getElementById('total').innerHTML;
+		let totalNum = parseFloat(total);
+		let updateTotal = totalNum - 1219 -5;
+		// let subtotal = document.getElementById('subtotal').innerHTML;
+		document.getElementById('total').innerHTML = updateTotal
+		console.log(updateTotal)
+
 	}
 }
 
@@ -133,6 +184,16 @@ function coverMinusBtnFunc(){
 		let phonePrice = document.getElementById('phonePrice').innerHTML;
 
 		subtotalCalc(phonePrice, coverPrice, -59)*/
+		//tax
+		let tax = document.getElementById('tax').innerHTML;
+
+		// total
+		let total = document.getElementById('total').innerHTML;
+		let totalNum = parseFloat(total);
+		let updateTotal = totalNum - 59 ;
+		// let subtotal = document.getElementById('subtotal').innerHTML;
+		document.getElementById('total').innerHTML = updateTotal
+		console.log(updateTotal)
 	}
 }
 
@@ -151,4 +212,46 @@ coverPlusBtn.addEventListener('click', function(){
 })
 coverMinusBtn.addEventListener('click', function(){
 	coverMinusBtnFunc();
+})
+
+// remove button alert
+let phoneRemove = document.getElementById('phoneRemove');
+let coverRemove = document.getElementById('coverRemove');
+
+function removeItem(){
+	alert('You should buy an iPhone')
+}
+
+phoneRemove.addEventListener('click', removeItem)
+coverRemove.addEventListener('click', removeItem)
+
+//login
+document.getElementById('checkOut').addEventListener('click', function(){
+	document.getElementById('main').style.display = 'none'
+	document.getElementById('login').style.display = 'block'
+})
+//buy button
+document.getElementById('buyBtn').addEventListener('click', function(){
+
+
+	let nameInput = document.getElementById('nameInput');
+	let passInput = document.getElementById('passInput');
+	if(nameInput.value == '' || nameInput.value.length < 2 || passInput.value == "" || passInput.value.length < 2){
+		console.log('Invalid')
+		alert("Enter Your name or Password")
+	}
+	else{
+		document.getElementById('main').style.display = 'block'
+		document.getElementById('login').style.display = 'none'
+		document.getElementById('buyer').innerHTML =`Congratulation! ${nameInput.value} <br> View Your Products`
+		nameInput.value = '';
+		passInput.value = '';
+
+	}
+
+})
+//close btn
+document.getElementById('closeBtn').addEventListener('click', function(){
+	document.getElementById('main').style.display = 'block';
+	document.getElementById('login').style.display = 'none'
 })
